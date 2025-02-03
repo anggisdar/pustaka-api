a698f9c41fc9e85f70c8d515fbdf75ad931a8cf4
anggis <anggisdar@gmail.com>
2025-02-03 16:05:53 +0700

    feat : git add query param for titile, price and  quantity

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", rootHandler)
-
+	router.GET("/", rootHandler) // root url memanggil fungsi rootHandler yang berisi JSON
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", booksHandler)
+	router.GET("/query", queryHandler)
 
 	// // root url "/"
 	// router.GET("/", func(c *gin.Context) {
@@ -44,4 +45,24 @@ func helloHandler(c *gin.Context) {
 		"Content":  "Perubahan kecil",
 		"subtitle": "Belajar Golang bareng Agung Setiawan",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id") // tangkap id
+
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+// func queryHandler(c *gin.Context) {
+// 	title := c.Param("title") // tangkap title
+
+// 	c.JSON(http.StatusOK, gin.H{"title": title})
+// }
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	quantity := c.Query("quantity")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price, "entity": quantity})
 }
